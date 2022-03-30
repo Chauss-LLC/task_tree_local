c3e7a76eb343b6802017c947c263cd0dcc3e26b9
Alex Poklonsky <jispar@turbanov.ru>
2022-03-30 08:06:15 +0300

    Refactor `Connection` class.
    * Rename `id` to `_id`, because it is assumed to be a read-only member.
    * Move string literals to constants.
    * Fix bug in `remove_tag()`.
    * Make second parameter in `__init__()` to be optional,
    so the creation of the Connection is now a little bit easier.

--- tasktree/core/connection.py
+++ tasktree/core/connection.py
@@ -7,20 +7,28 @@ from tasktree.core.system import TaskSystem
 class Connection:
     """Connection between tasks."""
 
-    id: str = ""
+    _id: str = ""
     tags: set = set()
+
+    CHILD_TAG: str = "child"
+    PARENT_TAG: str = "parent"
+    SELF_TAG: str = "self"
+
+    def get_id(self) -> str:
+        """Returns the id of a connected task."""
+        return self._id
 
     def is_child(self) -> bool:
         """Returns True if the connection have a 'child' tag."""
-        return self.have_tag("child")
+        return self.have_tag(Connection.CHILD_TAG)
 
     def is_parent(self) -> bool:
         """Returns True if the connection have a 'parent' tag."""
-        return self.have_tag("parent")
+        return self.have_tag(Connection.PARENT_TAG)
 
     def is_self(self) -> bool:
         """Returns True if the connection have a 'self' tag."""
-        return self.have_tag("self")
+        return self.have_tag(Connection.SELF_TAG)
 
     def have_tag(self, tag: str) -> bool:
         """Chack that the connection have a specified tag."""
@@ -35,17 +43,17 @@ class Connection:
 
     def remove_tag(self, tag: str) -> bool:
         """Remove the tag. Return if the flag was removed."""
-        if self.have_tag(tag):
+        if not self.have_tag(tag):
             return False
         self.tags.remove(tag)
         return True
 
-    def __init__(self, task_id: str, tags: Iterable):
+    def __init__(self, task_id: str, tags: Iterable = []):
         """Create a new connection with task id and specified tags."""
         if len(task_id) < TaskSystem.MINIMAL_TASK_ID_LENGTH:
             raise ValueError(
                 "Task id must be at least "
                 f"{TaskSystem.MINIMAL_TASK_ID_LENGTH} characters length."
             )
-        self.id = task_id
+        self._id = task_id
         self.tags = set(tags)
