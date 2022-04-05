481a7f480934271631bbef35fe592c4cb30e218b
Alex Poklonsky <jispar@turbanov.ru>
2022-04-05 14:40:01 +0300

    Add status default argument in `Task.__init__`.

--- tasktree/core/__init__.py
+++ tasktree/core/__init__.py
@@ -1,11 +1,12 @@
 """Core module of task_tree project."""
 
 from anytree import NodeMixin
+from tasktree.core.status import STATUS
 
 class Task(NodeMixin):
     """Task class for task tree."""
 
-    def __init__(self, name, status, parent=None, children=None):
+    def __init__(self, name, status: STATUS = STATUS.PENDING, parent=None, children=None):
         """Constructor for a new Task."""
         self.name = name
         self.status = status
