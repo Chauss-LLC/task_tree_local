ca70fb6f86dbc576de69e264456a449258e0bc6b
Alex Poklonsky <jispar@turbanov.ru>
2022-04-05 14:35:00 +0300

    Create base Task class.

--- tasktree/core/__init__.py
+++ tasktree/core/__init__.py
@@ -1,1 +1,14 @@
 """Core module of task_tree project."""
+
+from anytree import NodeMixin
+
+class Task(NodeMixin):
+    """Task class for task tree."""
+
+    def __init__(self, name, status, parent=None, children=None):
+        """Constructor for a new Task."""
+        self.name = name
+        self.status = status
+        self.parent = parent
+        if children:
+            self.children = children
